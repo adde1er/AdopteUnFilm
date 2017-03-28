92bf9584df999beac728f224c7d2685ed1cffa78
MissAccro / Julie Dell <accrox13@gmail.com>
2017-03-28 17:32:44 +0200

    laison des boutons aux pages correspondantes

---
+++ app/src/main/java/com/adopteunfilm/Activities/ProfilUser.java
@@ -0,0 +1,18 @@
+package com.adopteunfilm.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.adopteunfilm.R;
+
+/**
+ * Created by Utilisateur on 28/03/2017.
+ */
+
+public class ProfilUser extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_page);
+    }
+}
---
+++ app/src/main/java/com/adopteunfilm/Activities/RechercheFilms.java
@@ -0,0 +1,18 @@
+package com.adopteunfilm.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.adopteunfilm.R;
+
+/**
+ * Created by Adrien on 02/03/2017.
+ */
+
+    public class RechercheFilms extends AppCompatActivity {
+        protected void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+          setContentView(R.layout.recherche_films);
+        }
+}
