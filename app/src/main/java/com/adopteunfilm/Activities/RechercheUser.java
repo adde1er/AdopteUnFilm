afebdb8a74880d0b52e256dcb2aaa50d0de4e3d0
BOTTalos <alexandre.brizion@gmail.com>
2017-03-30 15:37:02 +0200

    recherche_films_users_1

--- app/src/main/java/com/adopteunfilm/Activities/RechercheUser.java
+++ app/src/main/java/com/adopteunfilm/Activities/RechercheUser.java
@@ -1,7 +1,12 @@
 package com.adopteunfilm.Activities;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.adopteunfilm.R;
 
@@ -10,9 +15,55 @@ import com.adopteunfilm.R;
  */
 
 public class RechercheUser extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+
+    public ListView lv_user;
+    public SearchView sv_user;
+
+    String[] test = {"test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10"};
+    ArrayAdapter<String> adapter_user;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recherche_users);
+        handleIntent(getIntent());
+
+        lv_user = (ListView) findViewById(R.id.listview_films);
+        sv_user = (SearchView) findViewById(R.id.searchview_films);
+
+        adapter_user = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,test);
+        lv_user.setAdapter(adapter_user);
+
+        sv_user.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String text) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String text) {
+
+                adapter_user.getFilter().filter(text);
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    private void doMySearch(String query) {
     }
 }
